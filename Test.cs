69ade5d3aa37b6b1329077744ac7c5dc80e81d2e
Your Name <you@example.com>
2026-06-12 21:36:18 +0300

    this is a test

---
+++ Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExaminationSystem
+{
+    internal class Test
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
